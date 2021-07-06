f73d4d1c0a4b41ea0f42918205e48545aa9c636d
Alphaalimamy <alphaalimamykamara@gmail.com>
2021-07-06 17:39:13 +0800

    second values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
 
 function App() {
+  const title = "Welcome to Simple Code Technologies";
+
+  const details = {
+    students: 50,
+    address: "Freetown",
+    founder: "Alpha Alimamy Kamara",
+  };
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>The Institution has {details.students} students</p>
+        <p>Address: {details.address}</p>
+        <p>Founder: {details.founder}</p>
       </div>
     </div>
   );
