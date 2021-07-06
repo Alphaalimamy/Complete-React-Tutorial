ad221987a5a859f1a229fe61ed467e612291cb35
Alphaalimamy <alphaalimamykamara@gmail.com>
2021-07-07 00:21:16 +0800

    Final code

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function Home() {
-  return <div className="home">Home</div>;
+  const handleClick = (e) => {
+    console.log("You clicked me", e.target);
+  };
+
+  const handleClickAgain = (name) => {
+    console.log(`Hello ${name}`);
+  };
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+
+      <button onClick={() => handleClickAgain("Alpha")}>Click Again</button>
+    </div>
+  );
 }
