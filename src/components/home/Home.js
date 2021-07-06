c1b6c9eabe5ef4a643eb61046d5f18a241dc7f71
Alphaalimamy <alphaalimamykamara@gmail.com>
2021-07-07 05:39:49 +0800

    State age  commit

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,13 +2,17 @@ import React from "react";
 import { useState } from "react";
 export default function Home() {
   const [name, setName] = useState("Alpha");
+  const [age, setAge] = useState(30);
   const handleClick = () => {
     setName("Rugiatu");
+    setAge(25);
   };
 
   return (
     <div className="home">
-      <h2>{name}</h2>
+      <h2>
+        {name} is {age} years old
+      </h2>
       <button onClick={handleClick}>Click me</button>
     </div>
   );
