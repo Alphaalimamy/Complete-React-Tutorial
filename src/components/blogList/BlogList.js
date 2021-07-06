949d54c90d764a65787f0ab87e21234fcf854354
Alphaalimamy <alphaalimamykamara@gmail.com>
2021-07-07 07:04:11 +0800

    final update

--- src/components/blogList/BlogList.js
+++ src/components/blogList/BlogList.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function BlogList(props) {
-  const { blogs, title } = props;
+  const { blogs, title, handleDelete } = props;
 
   return (
     <div className="blog-list">
@@ -10,6 +10,7 @@ export default function BlogList(props) {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
+          <button onClick={() => handleDelete(blog.id)}>delete blog</button>
         </div>
       ))}
     </div>
