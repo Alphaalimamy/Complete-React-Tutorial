dbd8acc0022f2ca9d4f073a08694fafeb4165da8
Alphaalimamy <alphaalimamykamara@gmail.com>
2021-07-07 06:03:53 +0800

    final output commit

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,19 +1,39 @@
 import React from "react";
 import { useState } from "react";
+import "./home.css";
+
 export default function Home() {
-  const [name, setName] = useState("Alpha");
-  const [age, setAge] = useState(30);
-  const handleClick = () => {
-    setName("Rugiatu");
-    setAge(25);
-  };
+  const [blogs, setBlogs] = useState([
+    {
+      id: 1,
+      title: "My new website",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, perspiciatis!",
+      author: "Alpha",
+    },
+
+    {
+      id: 2,
+      title: "Welcome party",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, perspiciatis!",
+      author: "Monarchy",
+    },
+
+    {
+      id: 3,
+      title: "Web dev top tips",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, perspiciatis!",
+      author: "Alpha",
+    },
+  ]);
 
   return (
     <div className="home">
-      <h2>
-        {name} is {age} years old
-      </h2>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 }
