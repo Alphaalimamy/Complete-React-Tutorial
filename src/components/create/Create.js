c29e5ecd7663f2daa22fb0f0ca094b7ba9ca7721
Alphaalimamy <alphaalimamykamara@gmail.com>
2021-07-08 07:32:17 +0800

    all

--- src/components/create/Create.js
+++ src/components/create/Create.js
@@ -1,7 +1,37 @@
+import { useState } from "react";
+import "./create.css";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Alpha");
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form action="">
+        <label>Blog Title</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label>Blog body</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+
+        <label>Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="Alpha">Alpha</option>
+          <option value="Rugiatu">Rugiatu</option>
+        </select>
+
+        <button>Add blog</button>
+      </form>
     </div>
   );
 };
